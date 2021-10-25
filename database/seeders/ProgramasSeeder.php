cb5783e8a6ce3331043c26399f423b4a103752cc
Diego <Dhbautista.r@gmail.com>
2021-10-25 03:12:44 -0500

    Seeders para crear usuario y programas

---
+++ database/seeders/ProgramasSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProgramasSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('programas')->insert([
+            [
+                'nombre'     => 'Bachillerato',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ],
+            [
+                'nombre'     => 'Inglés',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ],
+            [
+                'nombre'     => 'Preicfes',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ]
+        ]);
+    }
+}
